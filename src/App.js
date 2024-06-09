0f3b4f90b81c3267ebdfa2cd02038f174658b12e
Amit Saini <amitssaini999@gmail.com>
2024-06-09 16:45:33 +0530

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,82 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   const [countries, setCountries] = useState([]);
+//   const [filtered, setFiltered] = useState([]);
+//   const [search, setSearch] = useState("");
+
+//   const handleChange = (e) => {
+//     setSearch(e.target.value);
+//   };
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const resp = await fetch("https://restcountries.com/v3.1/all");
+//         const data = await resp.json();
+//         setCountries(data);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   useEffect(() => {
+//     const data = countries.filter((country) =>
+//       country.name.common.toLowerCase().includes(search.toLowerCase())
+//     );
+//     setFiltered(data);
+//   }, [search]);
+
+//   console.log(countries);
+//   return (
+//     <div>
+//       <div className="inp">
+//         <input
+//           type="text"
+//           placeholder="Enter a country"
+//           onChange={(e) => handleChange(e)}
+//         />
+//       </div>
+//       <div className="App">
+//         <div className="countryCard">
+//         {search === ""
+//           ? countries.map((country) => {
+//               return (
+                
+//                 <div>
+//                   <img src={country.flags.png} alt={country.flag}></img>
+//                   <p>{country.name.common}</p>
+//                 </div>
+                
+//               );
+//             })
+//           : filtered.map((country) => {
+//               return (
+                
+//                 <div >
+//                   <img src={country.flags.png} alt={country.flag}></img>
+//                   <p>{country.name.common}</p>
+//                 </div>
+                
+//               );
+//             })}
+//             </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
@@ -29,42 +108,27 @@ function App() {
       country.name.common.toLowerCase().includes(search.toLowerCase())
     );
     setFiltered(data);
-  }, [search]);
+  }, [search, countries]);
 
-  console.log(countries);
   return (
     <div>
       <div className="inp">
         <input
           type="text"
           placeholder="Enter a country"
-          onChange={(e) => handleChange(e)}
+          onChange={handleChange}
+          value={search}
         />
       </div>
       <div className="App">
         <div className="countryCard">
-        {search === ""
-          ? countries.map((country) => {
-              return (
-                
-                <div>
-                  <img src={country.flags.png} alt={country.flag}></img>
-                  <p>{country.name.common}</p>
-                </div>
-                
-              );
-            })
-          : filtered.map((country) => {
-              return (
-                
-                <div >
-                  <img src={country.flags.png} alt={country.flag}></img>
-                  <p>{country.name.common}</p>
-                </div>
-                
-              );
-            })}
+          {(search === "" ? countries : filtered).map((country) => (
+            <div key={country.cca3}>
+              <img src={country.flags.png} alt={country.name.common} />
+              <p>{country.name.common}</p>
             </div>
+          ))}
+        </div>
       </div>
     </div>
   );
