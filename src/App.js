a643e40b286f9cd357b21013676488eaf49e124f
Amit Saini <amitssaini999@gmail.com>
2024-06-09 17:01:07 +0530

    final-2

--- src/App.js
+++ src/App.js
@@ -77,58 +77,73 @@
 
 
 
-import logo from "./logo.svg";
-import "./App.css";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import CountriesSearch from './Component/CountriesSearch';
 
 function App() {
-  const [countries, setCountries] = useState([]);
-  const [filtered, setFiltered] = useState([]);
-  const [search, setSearch] = useState("");
+  const [countryData, setCountryData] = useState([]);
+  const [filterCountryData, setFilterCountryData] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   const handleChange = (e) => {
-    setSearch(e.target.value);
+    setSearchText(e.target.value);
+  };
+
+  const fetchCountryData = async () => {
+    let url = "https://restcountries.com/v3.1/all";
+    try {
+      let response = await axios.get(url);
+      setCountryData(response.data);
+      setFilterCountryData(response.data);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
   };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const resp = await fetch("https://restcountries.com/v3.1/all");
-        const data = await resp.json();
-        setCountries(data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
+    fetchCountryData();
   }, []);
 
+  const searchCountries = async () => {
+    if (searchText === "") {
+      setFilterCountryData(countryData);
+    }
+
+    let url = "https://restcountries.com/v3.1/all";
+
+    try {
+      let response = await axios.get(url);
+
+      const filteredData = response.data.filter((country) =>
+        country.name.common.toLowerCase().includes(searchText.toLowerCase())
+      );
+
+      setFilterCountryData(filteredData);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
   useEffect(() => {
-    const data = countries.filter((country) =>
-      country.name.common.toLowerCase().includes(search.toLowerCase())
-    );
-    setFiltered(data);
-  }, [search, countries]);
+    searchCountries();
+  }, [searchText]);
 
   return (
     <div>
-      <div className="inp">
-        <input
-          type="text"
-          placeholder="Enter a country"
-          onChange={handleChange}
-          value={search}
-        />
+      <div className="searchSection">
+        <form>
+          <input
+            type="text"
+            placeholder="Search for countries..."
+            value={searchText}
+            onChange={(e) => handleChange(e)}
+          />
+        </form>
       </div>
       <div className="App">
-        <div className="countryCard">
-          {(search === "" ? countries : filtered).map((country) => (
-            <div key={country.cca3}>
-              <img src={country.flags.png} alt={country.name.common} />
-              <p>{country.name.common}</p>
-            </div>
-          ))}
-        </div>
+        {filterCountryData &&
+          filterCountryData.map((ele) => <CountriesSearch data={ele} />)}
       </div>
     </div>
   );
